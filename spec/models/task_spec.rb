f0c69e45c3108e6919d0afeae9f6c9c27a678583
Riri0000 <ririko010102@gmail.com>
2020-09-23 19:19:22 +0900

    Fix:FactoryBot記述省略

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,26 +3,26 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validation' do
     it "is valid with all attributes" do
-      task = FactoryBot.create(:task)
+      task = build(:task)
       expect(task).to be_valid
       expect(task.errors).to be_empty
     end
 
     it "is invalid without a title" do
-      task = FactoryBot.build(:task, title: nil)
+      task = build(:task, title: nil)
       task.valid?
       expect(task.errors[:title]).to include("can't be blank")
     end
 
     it "is invalid without a status" do
-      task = FactoryBot.build(:task, status: nil)
+      task = build(:task, status: nil)
       task.valid?
       expect(task.errors[:status]).to include("can't be blank")
     end
 
     it "is invalid with a duplicate title" do
       FactoryBot.create(:task, title: "Title1")
-      task = FactoryBot.build(:task, title: "Title1")
+      task = build(:task, title: "Title1")
       task.valid?
       expect(task.errors[:title]).to include("has already been taken")
     end
